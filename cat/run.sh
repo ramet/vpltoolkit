bb062f9319ac06cae02cbf8fa93c8600259a5167
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-23 16:54:36 +0200

    add solution for cat

--- cat/run.sh
+++ cat/run.sh
@@ -3,20 +3,32 @@
 ### 0) load vplmodel
 echo "=> Run \"$EXO\" in mode $MODE with RUNDIR=$RUNDIR..."
 source $RUNDIR/vplmodel/vplmodel.sh
-ECHO "hello world!"
+source $RUNDIR/vpl_environment.sh
 
-env | grep VPL
-pwd
-ls
-cat vpl_environment.sh
+# $RUNDIR <-- you start here
+# $RUNDIR/vplmodel/*
+# $RUNDIR/GIT/$EXO/*
+
+ECHO "hello world!"
 
 ### 1) compilation
 ECHO "-COMPILATION"
 CFLAGS="-std=c99 -Wall"
-gcc $CFLAGS cat.c
+gcc $CFLAGS mycat.c -o mycat
 [ ! $? -eq 0 ] && ECHO "⚠ Compilation failure!" && exit 0
+
+cp $RUNDIR/GIT/$EXO/solution.c $RUNDIR
+gcc $CFLAGS solution.c -o solution
+[ ! $? -eq 0 ] && ECHO "⚠ VPL Script Error!" && exit 0
 
 ### 2) execution
 
 ECHO "-EXECUTION"
-./a.out
+
+echo "abcdef" | ./mycat > mycat.out
+[ ! $? -eq 0 ] && ECHO "⚠ Execution failure!" && exit 0
+echo "abcdef" | ./solution > solution.out
+[ ! $? -eq 0 ] && ECHO "⚠ VPL Script Error!" && exit 0
+
+diff mycat.out solution.out
+

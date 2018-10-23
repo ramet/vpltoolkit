e455177bc9366360867560cb952b810bd1c05f03
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-23 16:23:08 +0200

    compilation

--- cat/run.sh
+++ cat/run.sh
@@ -3,4 +3,13 @@
 echo "=> Run \"$EXO\" in mode $MODE with RUNDIR=$RUNDIR..."
 
 
+### 1) compilation
+ECHO "-COMPILATION"
+CFLAGS="-std=c99 -Wall"
+gcc $CFLAGS cat.c
+[ ! $? -eq 0 ] && ECHO "⚠ Compilation failure!" && exit 0
 
+### 2) execution
+
+ECHO "-EXECUTION"
+./a.out

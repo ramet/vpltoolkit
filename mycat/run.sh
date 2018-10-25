d2ffe804b3a37dd6884a5e49f655038fd945d863
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-25 18:10:10 +0200

    minor

--- mycat/run.sh
+++ mycat/run.sh
@@ -18,7 +18,6 @@ TRACE "gcc $CFLAGS mycat.c -o mycat"
 ECHO "✓ Success!"
 
 CFLAGS="-std=c99 -Wall"
-cp $RUNDIR/download/solution.c $RUNDIR
 gcc $CFLAGS solution.c -o solution
 [ ! $? -eq 0 ] && ECHO "⚠ Oups... VPL Script Error!" && exit 0
 

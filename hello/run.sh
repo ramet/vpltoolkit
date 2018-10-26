24f07fa88f61f729207d93b8fd9ea0eb0aafe873
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-26 17:01:57 +0200

    minor

--- hello/run.sh
+++ hello/run.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-### 0) load vplmodel
-echo "=> Run \"$EXO\" in mode $MODE with RUNDIR=$RUNDIR..."
-source $RUNDIR/vplmodel/toolkit.sh
-
-### 1) do your stuff
-ECHO "hello world!"
+echo "hello world!"
 
--- mycat/run.sh
+++ mycat/run.sh
@@ -1,36 +1,33 @@
 #!/bin/bash
 
-### 0) load vplmodel
+### 0) initialization
 source env.sh
 source vplmodel/toolkit.sh
-
 [ ! "$RUNDIR" = "$PWD" ] && echo "⚠ RUNDIR is not set correctly!" && exit 0
-
-### prepare & check inputs
 cd $RUNDIR
 CHECK "inputs/mycat.c"
-
 GRADE=0
 
 ### 1) compilation
 
-ECHO "-COMPILATION"
+ECHO "-COMPILATION" && ECHO
 
-CFLAGS="-std=c99 -Werror"
-# CFLAGS="-std=c99 -Wall"
+CFLAGS="-std=c99 -Wall"
 cp inputs/mycat.c $RUNDIR
-TRACE "gcc $CFLAGS mycat.c -o mycat"
+TRACE "gcc $CFLAGS mycat.c -o mycat &> warnings"
 [ ! $? -eq 0 ] && ECHO "⚠ Compilation failure!" && EXIT
-ECHO "✓ Success!" && SCORE 20
+[ ! -s warnings ] && ECHO "⚠ Compilation warnings!" && SCORE -20
+ECHO "✓ Success!" && SCORE 30
 
 ### 2) execution
 
-ECHO "-EXECUTION"
+ECHO "-EXECUTION" && ECHO
 
 echo "abcdef" > mycat.in
 cat mycat.in | ./mycat > mycat.out
 [ ! $? -eq 0 ] && ECHO "⚠ Execution failure!" && EXIT
 diff -q mycat.in mycat.out &> /dev/null
 [ ! $? -eq 0 ] && ECHO "⚠ Your program output is invalid!" && EXIT
-ECHO "✓ Success!" && SCORE 80
-EXIT
+ECHO "✓ Success!" && SCORE 70
+
+ECHO && EXIT

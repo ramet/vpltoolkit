5b12d32fea9c463c12b7bd601dfcc0f10c778596
Aurélien Esnard <aurelien.esnard@u-bordeaux.fr>
2018-10-30 09:26:41 +0100

    minor

--- mycat/run.sh
+++ mycat/run.sh
@@ -8,15 +8,11 @@ source vpltoolkit/toolkit.sh
 CHECKINPUTS
 COPYINPUTS
 
-### 1) compilation
-echo "-COMPILATION"
 CFLAGS="-std=c99 -Wall"
 TRACE "gcc $CFLAGS mycat.c -o mycat"
 [ ! $? -eq 0 ] && echo "⚠ Compilation failure!" && exit 0
-
-### 2) execution
-echo "-EXECUTION"
 TRACE "echo "abcdef" > mycat.in && cat mycat.in"
 TRACE "cat mycat.in | ./mycat | tee mycat.out"
 TRACE "diff -q mycat.in mycat.out"
 [ ! $? -eq 0 ] && echo "⚠ Your program output is invalid!" && exit 0
+echo "✓ Success!"
